b6f878ba6b49341d3b6f0aa7d349ad8b97aa036b
diezalina <alina.diezgr@gmail.com>
2024-09-09 22:25:17 +0200

    Initial commit for blog-app

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
+
+// https://coolors.co/590d22-800f2f-a4133c-c9184a-ff4d6d-ff758f-ff8fa3-ffb3c1-ffccd5-fff0f3
